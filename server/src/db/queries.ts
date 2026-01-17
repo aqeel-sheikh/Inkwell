a3b9d1d381a9b9f2a383950ce8680fa8cdc5e75e
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-17 15:45:09 +0530

    Refactor insertPost function to simplify error handling in blog post creation. Removed try-catch block for cleaner code structure.

--- server/src/db/queries.ts
+++ server/src/db/queries.ts
@@ -2,15 +2,10 @@ import { prisma } from "@/config/prisma";
 import type { BlogPostType } from "@/types/posts.schema";
 
 export const insertPost = async (postData: BlogPostType, userId: string) => {
-  try {
-    await prisma.blogPost.create({
-      data: {
-        ...postData,
-        authorId: userId,
-      },
-    });
-  } catch (error) {
-    console.error("Failed to insert blog post:", error);
-    throw new Error("Could not create blog post");
-  }
-};
+  await prisma.blogPost.create({
+    data: {
+      ...postData,
+      authorId: userId,
+    },
+     })
+}
