93a56533c740543d218b26723cd3df3a52845f9c
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-17 08:55:39 +0530

    Move prisma.ts file to config dir for better structure

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./prisma";
+import { prisma } from "@/config/prisma";
 import "dotenv/config";
 
 export const auth = betterAuth({
