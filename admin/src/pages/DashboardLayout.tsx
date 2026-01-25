24c51bf7d63a70e9b2af7353c99ff2a1ce072fff
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-25 22:51:14 +0530

    refactor: Improve responsive styling in DashboardLayout component

--- admin/src/pages/DashboardLayout.tsx
+++ admin/src/pages/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 
 export function DashboardLayout() {
   return (
-    <div className="py-2 pr-2 min-h-screen bg-[#f5f5f5] dark:bg-[#18181b]">
+    <div className="md:py-2 min-h-screen bg-[#f5f5f5] dark:bg-[#18181b]">
       <SidebarProvider>
         <AppSidebar />
         <main className="flex-1 bg-white dark:bg-[#09090b] rounded-2xl w-full">
