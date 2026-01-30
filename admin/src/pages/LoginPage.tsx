9f7cbd76dd26ba3faa2dcdd1ec47104b5888b842
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-30 15:22:09 +0530

    feat(LoginPage): enhance login interface with password visibility toggle and improved error handling
    - Added a password visibility toggle to enhance user experience during login.
    - Updated error message display with refined styling and animation.
    - Refactored layout and styling for a cleaner, more accessible design.

--- admin/src/pages/LoginPage.tsx
+++ admin/src/pages/LoginPage.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router";
 import { signIn } from "@/auth/authClient";
 import { Button, Input } from "@/components";
-import { BookOpen } from "lucide-react";
+import { BookOpen, TriangleAlert, Eye, EyeOff } from "lucide-react";
 
 export function LoginPage() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -38,27 +39,15 @@ export function LoginPage() {
   return (
     <>
       <div className="relative flex min-h-screen items-center justify-center bg-[#fafaf9] p-6">
-        {/* Ambient background */}
-        <div className="pointer-events-none fixed inset-0 overflow-hidden">
-          <div
-            className="absolute -right-64 -top-64 h-[800px] w-[800px] rounded-full bg-linear-to-br from-amber-100/40 via-rose-100/30 to-transparent opacity-60 blur-3xl animate-float"
-            style={{ animationDelay: "0s" }}
-          />
-          <div
-            className="absolute -bottom-64 -left-64 h-[700px] w-[700px] rounded-full bg-linear-to-tr from-indigo-100/40 via-purple-100/30 to-transparent opacity-50 blur-3xl animate-float"
-            style={{ animationDelay: "2s" }}
-          />
-        </div>
-
         <div className="relative w-full max-w-md animate-slideUp">
           <div className="overflow-hidden rounded-2xl border border-stone-200/80 bg-white/90 shadow-2xl backdrop-blur-xl">
             {/* Decorative top gradient */}
-            <div className="h-2 bg-linear-to-r from-amber-500 via-rose-500 to-purple-500" />
+            <div className="h-2 bg-stone-900" />
 
             <div className="p-10">
               {/* Logo */}
               <div className="mb-8 flex justify-center">
-                <div className="flex h-16 w-16 items-center justify-center rounded-2xl bg-linear-to-br from-amber-500 to-rose-500 shadow-2xl">
+                <div className="flex h-16 w-16 items-center justify-center rounded-2xl bg-stone-900">
                   <BookOpen className="h-8 w-8 text-white" strokeWidth={2} />
                 </div>
               </div>
@@ -81,21 +70,9 @@ export function LoginPage() {
 
               {/* Error message */}
               {error && (
-                <div className="mb-6 rounded-xl border-2 border-red-100 bg-red-50/80 p-4">
+                <div className="mb-3 rounded-xl border border-red-100 bg-red-50/80 p-2 animate-slideInRight">
                   <div className="flex items-start gap-3">
-                    <svg
-                      className="h-5 w-5 shrink-0 text-red-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                      />
-                    </svg>
+                    <TriangleAlert className="text-red-600 h-5 w-5" />
                     <p
                       className="text-sm text-red-800"
                       style={{ fontFamily: "'DM Sans', sans-serif" }}
@@ -122,24 +99,38 @@ export function LoginPage() {
                 />
 
                 {/* Password */}
-                <Input
-                  label="Password"
-                  type="password"
-                  value={formData.password}
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
-                  placeholder="••••••••"
-                  required
-                  className="border-stone-200 transition-all duration-300 focus:border-stone-400"
-                />
+                <div className="relative">
+                  <Input
+                    label="Password"
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    value={formData.password}
+                    onChange={(e) =>
+                      setFormData({ ...formData, password: e.target.value })
+                    }
+                    placeholder="••••••••"
+                    autoComplete="current-password"
+                    required
+                    className="border-stone-200 transition-all duration-300 focus:border-stone-400"
+                  />
+                  <div
+                    className="absolute top-[55%] right-2 text-stone-600 cursor-pointer hover:text-stone-800"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-5 w-5" />
+                    ) : (
+                      <Eye className="h-5 w-5" />
+                    )}
+                  </div>
+                </div>
 
                 {/* Submit Button */}
                 <Button
                   type="submit"
                   fullWidth
                   isLoading={isLoading}
-                  className="mt-6 border-0 bg-linear-to-br from-stone-900 via-stone-800 to-stone-900 py-3 font-medium text-white shadow-lg transition-all duration-500 hover:shadow-xl hover:scale-105"
+                  className="mt-6 border-0 bg-linear-to-br from-stone-900 via-stone-800 to-stone-900 py-3 font-medium text-white shadow-lg transition-all duration-500 hover:shadow-xl hover:scale-105 cursor-pointer"
                   style={{ fontFamily: "'DM Sans', sans-serif" }}
                 >
                   Sign In
@@ -154,7 +145,7 @@ export function LoginPage() {
                 Don't have an account?{" "}
                 <Link
                   to="/register"
-                  className="font-medium text-stone-900 transition-colors hover:text-stone-700"
+                  className="font-medium text-stone-900 transition-colors hover:underline"
                 >
                   Sign up
                 </Link>
