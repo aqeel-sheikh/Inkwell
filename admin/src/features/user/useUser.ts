c9244024f5ffecb59ee8526bd704ade1f21cdfa6
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-30 17:37:49 +0530

    refactor(useUser): update user mutation handling to utilize session refetch
    - Replaced navigation on successful user update with session refetch to ensure data consistency.
    - Improved user experience by ensuring the latest session data is reflected after updates.

--- admin/src/features/user/useUser.ts
+++ admin/src/features/user/useUser.ts
@@ -1,15 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { api } from "@/services/api";
 import type { User } from "@/types";
-import { useNavigate } from "react-router";
+import { useSession } from "@/auth/authClient";
 
 export function useUpdateUser() {
-  const navigate = useNavigate();
+  const {refetch} = useSession()
 
   return useMutation({
     mutationFn: (data: User) => api.user.update(data),
     onSuccess: () => {
-      navigate(0);
+      refetch()
     },
   });
 }
