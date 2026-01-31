5a791a713cb36171172bf00234e2048961b06968
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-31 16:19:59 +0530

    chore(root.controller): update response message to HTML format for API status

--- server/src/controllers/root.controller.ts
+++ server/src/controllers/root.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
 export const rootController = (req: Request, res: Response) => {
-  res.status(200).send("Members-only API is running");
+  res.status(200).send("<h1>Inkwell API is running<h1>");
 };
