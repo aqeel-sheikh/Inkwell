b8ed3a3dd8782807e3fe4ed6f95d6893a3da9d08
Aqeel Sheikh <sheikhakeelw01@gmail.com>
2026-01-31 15:18:57 +0530

    fix(server): update TypeScript configuration and server startup logic
    - Added 'tsc-alias' configuration to tsconfig.json for improved TypeScript path resolution.
    - Modified server.ts to conditionally start the server based on the VERCEL environment variable, enhancing deployment flexibility and logging clarity.

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = createApp();
 
-app.listen(PORT, (err: unknown) => {
-  if (err) console.error("Can't start the sever", err);
+if (process.env.VERCEL !== "1") {
+  app.listen(PORT, (err: unknown) => {
+    if (err) console.error("Can't start the server", err);
+    console.log(`Server is listening at Port:${PORT}`);
+  });
+}
 
-  console.log(`Server is listening at Port:${PORT}`);
-});
+export default app;
